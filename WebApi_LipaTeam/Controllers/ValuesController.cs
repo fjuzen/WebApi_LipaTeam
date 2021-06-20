5dd2e9e4cff87f30678fba9a83406e1ce741e3b0
fjuzen <59693505+fjuzen@users.noreply.github.com>
2021-06-20 13:37:18 +0200

    Add project files.

---
+++ WebApi_LipaTeam/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApi_LipaTeam.Models;
+
+namespace WebApi_LipaTeam.Controllers
+{
+    [AllowAnonymous]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [Route("api/get_response")]
+        [HttpPost]
+        public MessageModel Get_Response(MessageModel value)
+        {
+            MessageModel msg = new MessageModel();
+            if (value.Text != "")
+            {
+                msg.Text = "Reponse: " + value.Text;
+                return msg;
+            }
+            return msg;
+        }
+
+        [Route("api/get_response1")]
+        [HttpGet]
+        public async Task<string> Get_Response1()
+        {
+            var test = "aaa";
+            return test;
+        }
+    }
+}
